d3c4652f584ab007028088b6e9b3fd18d7cb10c8
Kelvin Araújo Ferreira <kelvin.ufrr@gmail.com>
2026-07-16 18:49:50 -0400

    Subindo atvs da aula 04

---
+++ aula04/atividade_4_ponta_a_ponta/test_e2e.py
@@ -0,0 +1,13 @@
+from fluxo import BancoReal, ControladorHTTP, InterfaceUsuario
+
+def test_fluxo_completo_ponta_a_ponta():
+    # 1. PREPARAÇÃO (Encaixe as peças de baixo para cima / Bottom-up!)
+    # banco = ...
+    # controlador = ...
+    # interface = ...
+    
+    # 2. AÇÃO (O usuário clica no botão salvar)
+    # resultado = interface.clicar_botao_salvar("Mouse Gamer")
+    
+    # 3. VALIDAÇÃO E2E (Valide se o 'resultado' é OK e se o 'Mouse Gamer' está dentro do banco.tabela!)
+    pass
